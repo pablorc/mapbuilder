4169cd8fafa8ad4c2bd305728ae517bfd452920d
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2017-05-06 12:23:16 +0200

    Use const instead of var

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-var MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicGFibG9yYyIsImEiOiJjajI3djNyOXAwMGR3MndzMWV2cjJicHo3In0.EIxpAD7wO3gmdkqt4ozKbg';
-var GEOJSON_URL = 'https://xavijam.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20ne_10m_populated_places_simple&format=GeoJSON';
-var MAP_DOM_ID = 'map';
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicGFibG9yYyIsImEiOiJjajI3djNyOXAwMGR3MndzMWV2cjJicHo3In0.EIxpAD7wO3gmdkqt4ozKbg';
+const GEOJSON_URL = 'https://xavijam.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20ne_10m_populated_places_simple&format=GeoJSON';
+const MAP_DOM_ID = 'map';
 
-var Map = function(layers) {
+const Map = function(layers) {
   this.layers = layers;
+  layers.subscribe(this);
 };
+
+Map.prototype.notify = function(event, subject) {
+  this.addLayer(subject);
+}
 
 Map.prototype.render = function(domID) {
   this.map = L.map(domID).setView([5,0], 2);
@@ -18,7 +23,7 @@ Map.prototype.addLayer = function(layer) {
   L.geoJSON(layer.toGeoJSON()).addTo(this.map);
 };
 
-var Point = function(attrs) {
+const Point = function(attrs) {
   this.attrs = attrs;
 };
 
@@ -26,7 +31,7 @@ Point.prototype.toGeoJSON = function() {
   return this.attrs;
 }
 
-var Layer = function(layer) {
+const Layer = function(layer) {
   this.layer = layer;
 }
 
@@ -37,71 +42,68 @@ Layer.prototype.toGeoJSON = function() {
   };
 }
 
-var SelectFeatureView = function(feature, onToggleCallback) {
+const SelectFeatureView = function(feature, onToggleCallback) {
   this.feature = feature;
   this.onToggleCallback = onToggleCallback;
 }
 
 SelectFeatureView.prototype.render = function() {
-  var li = document.createElement('li');
-  var textNode = document.createTextNode(this.feature.attrs.properties.name);
+  const li = document.createElement('li');
+  const textNode = document.createTextNode(this.feature.attrs.properties.name);
   li.appendChild(textNode);
   li.addEventListener('click', () => this.onToggleCallback(this.feature));
   return li;
 }
 
-var ListView = function(items) {
+const ListView = function(items) {
   this.items = items;
 }
 
 ListView.prototype.render = function(domId, onToggleCallback, itemBuilder) {
-  var renderedPoints = this.items.map((item) => itemBuilder(item).render());
-  var ul = document.createElement('ul');
-  var append = (child) => ul.appendChild(child);
+  const renderedPoints = this.items.map((item) => itemBuilder(item).render());
+  const ul = document.createElement('ul');
+  const append = (child) => ul.appendChild(child);
   renderedPoints.map((child) => append(child));
   document.getElementById(domId).appendChild(ul);
 }
 
-var Publisher = function() {
+const Publisher = function() {
   let handlers = [];
 
   const that = new Object();
   that.subscribe = (observer) => handlers.push(observer);
-  that.notify = (objectChanged) => handlers.map((handler) => handler.notify(objectChanged))
+  that.notify = (...args) => handlers.map((handler) => handler.notify(...args))
 
   return that;
 }
 
-
-var Layers = function() {
+const Layers = function() {
   const layers = [];
 
   const that = Object.create(new Publisher());
 
   that.add = (layer) => {
     layers.push(layer);
-    this.notify('layer.added', layer);
+    that.notify('layer.added', layer);
   };
 
   return that;
 };
 
-
-
 const start = (geojson) => {
-  var features = geojson.features.map((feature) => new Point(feature));
-  var layers = [];
-  var map = new Map(layers);
+  const features = geojson.features.map((feature) => new Point(feature));
+  const layers = Layers();
+  const map = new Map(layers);
   map.render(MAP_DOM_ID);
 
-  var onToggleCallback = (selectedFeature) => map.addLayer(new Layer([selectedFeature]));
+  const onToggleCallback = (selectedFeature) => layers.add(new Layer([selectedFeature]));
 
-  var selectFeatureViewBuilder = (feature) => new SelectFeatureView(feature, onToggleCallback);
+  const selectFeatureViewBuilder = (feature) => new SelectFeatureView(feature, onToggleCallback);
   new ListView(features).render('features', onToggleCallback, selectFeatureViewBuilder);
 
-  var layerViewBuilder = (layer) => new SelectFeatureView(feature, () => {});
+  const layerViewBuilder = (layer) => new SelectFeatureView(feature, () => {});
   new ListView(layers).render('layers', onToggleCallback, selectFeatureViewBuilder);
 }
 
-//self.fetch(GEOJSON_URL)
-//  .then((response) => response.json().then(start));
+self.fetch(GEOJSON_URL)
+  .then((response) => response.json().then(start));
